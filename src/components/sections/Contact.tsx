de4751b01ae8c16543e15136708a853a80bff1a1
Muhammad <muhammadtaufiq2097@gmail.com>
2026-07-18 09:28:32 +0700

    Change target email to taufiqq882@gmail.com for testing

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -21,7 +21,7 @@ export function Contact() {
 
   // Data Kontak Pemilik
   const ownerWhatsApp = "62895359530117"; // Format 62 tanpa + atau 0 di depan
-  const ownerEmail = "salahudinkoliq10@gmail.com";
+  const ownerEmail = "taufiqq882@gmail.com";
 
   const getMessageText = () => {
     let text = `Halo tim S-TechX,\n`;
